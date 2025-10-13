e09b663c2f27150ee73ca1644b13ac1d617f46e3
sohail272007 <sohailshaikh87666@eng.rizvi.edu.in>
2025-10-13 09:49:34 +0530

    Create EXP 1a.C

---
+++ EXP 1a.C
@@ -0,0 +1,17 @@
+/*
+name: sohail shaikh
+
+UIN: 251M027
+
+class: FE mechanics
+
+*/
+
+#include <stdio.h>
+
+int main() {
+
+printf("1) My\bname is sohail.\n2) My age is 18.\n3) First year engineering.\n4) My division is (F).\n5) My UIN number is 251M027.");
+
+return 0; 
+}
