8f7fc19b1e04c74324cebb2ba97c1daa39c23232
sohail272007 <sohailshaikh87666@eng.rizvi.edu.in>
2025-10-13 09:48:09 +0530

    Create exp 1a.C

---
+++ exp 1a.C
@@ -0,0 +1,43 @@
+/*
+name: sohail shaikh
+
+UIN: 251M027
+
+class: FE mechanics
+*/
+
+#include <stdio.h>
+
+int main()
+
+int maths ,chem ,phy , sum;
+
+',sum);
+
+printf("enter maths marks \n");
+
+scanf("%d", & maths);
+
+printf("enter chem marks\n");
+
+scanf("%d", & chem);
+
+printf("enter phy marks\n");
+
+scanf("%d", & phy);
+
+sum=maths+chem+phy;
+
+printf("total marks is\n%d
+
+float avr;
+
+avr=(float)sum/3;
+
+printf("\naverag is\n%2f ",avr);
+
+(avr>=50)? printf("\nstudent is pass"):printf("\nstudent
+
+fail");
+
+return 0;}
