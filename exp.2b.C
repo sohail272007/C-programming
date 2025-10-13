51b617d4280144bb524783cd020769e29085cfe8
sohail272007 <sohailshaikh87666@eng.rizvi.edu.in>
2025-10-13 09:46:54 +0530

    Update and rename README.md to exp.2b.C

---
+++ exp.2b.C
@@ -0,0 +1,36 @@
+/*sohail shaikh
+
+UIN 251M027
+
+branch mechanics*/
+
+#include <stdio.h>
+
+int main() {
+
+int start, end, i, sum = 0:
+
+printf("enter starting number");
+
+scanf("%d",&start);
+
+printf("enter ending number"):
+
+scanf("%d", &end):
+
+for(int i=start;i<=end;i++)
+{
+
+if(i%2!-0)
+
+{
+
+printf("%d",i);
+
+sum=sum+i:
+}
+}
+
+printf("sum of all odd number is %d\n", sum);
+
+return 0:}
