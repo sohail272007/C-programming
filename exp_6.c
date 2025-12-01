09192229762f4a0f864edb25066af7503b45ffdb
sohail272007 <sohailshaikh87666@eng.rizvi.edu.in>
2025-12-01 14:15:27 +0530

    Create exp_6.c

---
+++ exp_6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void counter() {
+    static int count = 0;  
+    count++;               
+    printf("Function called %d times\n", count);
+}
+
+int main() {
+    counter();
+    counter();
+    counter();
+    counter();  
+    return 0;
+}
