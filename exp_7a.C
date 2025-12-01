2838b4c09e3333ab69148d67f7601ee0d5696259
sohail272007 <sohailshaikh87666@eng.rizvi.edu.in>
2025-12-01 14:16:37 +0530

    Create exp_7a.C

---
+++ exp_7a.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    float arr[100], max;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%f", &arr[i]);
+    }
+
+    max = arr[0];  
+
+    for (i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    printf("The largest element in the array is: %.2f\n", max);
+
+    return 0;
+}
